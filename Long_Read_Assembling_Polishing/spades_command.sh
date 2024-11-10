0b794c792102a5c3e8c1fe86f8d384cc6f1b0959
SandersonHaley <54908689+SandersonHaley@users.noreply.github.com>
2024-11-10 15:59:49 -0600

    Add files via upload

---
+++ Long_Read_Assembling_Polishing/spades_command.sh
@@ -0,0 +1,1 @@
+spades.py -t 15 -m 150 -1 "$line"/"$line"_SR_1.trimmed.fastq -2 "$line"/"$line"_SR_2.trimmed.fastq --trusted-contigs "$line"/flye_output/assembly.fasta -o "$line"/"$line"_spades_hybrid_assembly
